968bb281415e0d453ebbd205ff6533a193b84b85
Luiserebii <luis@serebii.io>
2016-01-29 01:38:32 -0500

    Added random pokemon and sprites, which I will update for reasons. May consider adding Pokemon and move databases next, maybe ripped from Showdown, and working out the logical side of battling.

--- src/application/Battle.java
+++ src/application/Battle.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Random;
+
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
@@ -14,8 +16,9 @@ public class Battle { //Really messy class, needs to be developed
 	Image battleBox = new Image("file:images/battle_box.png");
 
 	Image emeraldBattle1 = new Image("file:images/emerald_battle_1.png");
-	
-	Image pokemon = new Image("file:images/1_scaled.gif");
+
+	Image pokemon = new Image("file:images/261_scaled.gif");
+	int num;
 
 	Image HPBox1 = new Image("file:images/hp_box_1.png"); //goes on the right - main
 	Image HPBox2 = new Image("file:images/hp_box_2.png"); //
@@ -36,8 +39,9 @@ public class Battle { //Really messy class, needs to be developed
 
 		t1 = System.nanoTime();
 		isBattleStart = true;
-		g1x = 732;//720, 
+		g1x = 732;//720,
 		g2x = -384; //384. + 21 if @ 3
+		randomPokemon();
 	}
 
 	public void updateBattle(GraphicsContext gc){
@@ -63,7 +67,8 @@ public class Battle { //Really messy class, needs to be developed
 		gc.drawImage(grassBattleBackground,0,0);
 		gc.drawImage(grassBattleGrass1,g1x,303);
 		gc.drawImage(grassBattleGrass2,g2x,144);
-		gc.drawImage(pokemon,g2x+105,94);
+		int pokemonOffset = (int) (grassBattleGrass2.getWidth() - pokemon.getWidth())/2;
+		gc.drawImage(pokemon,g2x+pokemonOffset,215-pokemon.getHeight());//87 //g2x+90.5,104 //decided just to do simple alg, too complicated to be precise
 		gc.drawImage(emeraldBattle1,g1x + 186,180);
 
 
@@ -91,7 +96,12 @@ public class Battle { //Really messy class, needs to be developed
 
 
 
+	public void randomPokemon(){
 
+		num = new Random().nextInt(386) + 1;
+		pokemon = new Image("file:images/pokemon_sprites/" + num + ".gif_.gif");
+
+	}
 
 
 
