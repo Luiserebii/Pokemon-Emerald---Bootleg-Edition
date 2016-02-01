fad70a47b7373ac0124a6528a1790ee3193dcba5
Jire <technology@live.com>
2016-01-31 21:44:37 -0500

    Now uses `isPresent` rather than throwing illegal state (logic error)

--- src/application/GameLoop.java
+++ src/application/GameLoop.java
@@ -38,25 +38,24 @@ class GameLoop extends AnimationTimer {
 			TileEngine te = new TileEngine();
 			te.generateTiles(gc);
 
-			Direction dir = of(Direction.cachedValues).filter(v -> input.contains(v.name())).findFirst()
-					.orElseThrow(() -> new IllegalStateException("Could not find direction for input \"" + input + "\""));
+			of(Direction.cachedValues).filter(v -> input.contains(v.name())).findFirst().ifPresent(dir -> {
+				t2 = System.nanoTime();
+				diff = t2 - t1; //check time elapsed, reset t1 if gets too late
+				//System.out.println(diff);
+				t2 = System.nanoTime();
+				diff = t2 - t1;
 
-			t2 = System.nanoTime();
-			diff = t2 - t1; //check time elapsed, reset t1 if gets too late
-			//System.out.println(diff);
-			t2 = System.nanoTime();
-			diff = t2 - t1;
+				if (diff < interval) gc.drawImage(dir.image1.apply(e), e.posX, e.posY, e.width, e.height);
+				if (diff > interval && diff < interval * 2) gc.drawImage(dir.image.apply(e), e.posX, e.posY, e.width, e.height);
+				if (diff > interval * 2 && diff < interval * 3) gc.drawImage(dir.image2.apply(e), e.posX, e.posY, e.width, e.height);
+				if (diff > interval * 3) gc.drawImage(dir.image.apply(e), e.posX, e.posY, e.width, e.height);
+				if (diff > interval * 4) t1 = System.nanoTime();
+				dir.move.accept(e);
+				e.direction = dir;
 
-			if (diff < interval) gc.drawImage(dir.image1.apply(e), e.posX, e.posY, e.width, e.height);
-			if (diff > interval && diff < interval * 2) gc.drawImage(dir.image.apply(e), e.posX, e.posY, e.width, e.height);
-			if (diff > interval * 2 && diff < interval * 3) gc.drawImage(dir.image2.apply(e), e.posX, e.posY, e.width, e.height);
-			if (diff > interval * 3) gc.drawImage(dir.image.apply(e), e.posX, e.posY, e.width, e.height);
-			if (diff > interval * 4) t1 = System.nanoTime();
-			dir.move.accept(e);
-			e.direction = dir;
-
-			int randInt = ThreadLocalRandom.current().nextInt(2000) + 1;
-			if (randInt < 10) isBattle = true;
+				int randInt = ThreadLocalRandom.current().nextInt(2000) + 1;
+				if (randInt < 10) isBattle = true;
+			});
 
 			if (input.size() == 0) gc.drawImage(e.direction.image.apply(e), e.posX, e.posY, e.width, e.height);
 		}
